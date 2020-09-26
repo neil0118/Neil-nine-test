5dc90c886346aebfa4c089c0157958097a288296
Neil Hu <hysh1991@gmail.com>
2020-09-26 12:47:00 +1000

    build simple get/post request to root/"/"

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const rootPost = (req, res) => {
+  console.log(req.body.payload);
+  res.send("POST request to the homepage");
+};
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.json()); // receive json data from Post request
+app.get("/", (req, res) => res.render("pages/index"));
+app.post("/", (req, res) => rootPost(req, res));
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
